55f70387618e6722adc8edf07fd5ce4ceeb07261
Thanat Mathanukraw <phoon884@gmail.com>
2022-07-25 17:00:41 +0700

    q

--- core/raft.py
+++ core/raft.py
@@ -61,8 +61,8 @@ class RAFT(nn.Module):
     def initialize_flow(self, img):
         """ Flow is represented as difference between two coordinate grids flow = coords1 - coords0"""
         N, C, H, W = img.shape
-        print(f"check here{N} and {coords0.shape}")
         coords0 = coords_grid(N, H//8, W//8).to(img.device)
+        print(f"check here{N} and {coords0.shape}")
         coords1 = coords_grid(N, H//8, W//8).to(img.device)
 
         # optical flow computed as difference: flow = coords1 - coords0
