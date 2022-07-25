d5da189187eb46077c821276f55a062b23a6becc
Thanat Mathanukraw <phoon884@gmail.com>
2022-07-25 17:16:07 +0700

    what is this

--- core/corr.py
+++ core/corr.py
@@ -34,9 +34,9 @@ class CorrBlock:
 
             centroid_lvl = coords.reshape(batch*h1*w1, 1, 1, 2) / 2**i
             delta_lvl = delta.view(1, 2*r+1, 2*r+1, 2)
-            print(f"delta level shape: {delta_lvl.shape}")
             coords_lvl = centroid_lvl + delta_lvl
 
+            print(f"delta level shape: {coords_lvl.shape}")
             corr = bilinear_sampler(corr, coords_lvl)
             corr = corr.view(batch, h1, w1, -1)
             out_pyramid.append(corr)
