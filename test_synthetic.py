f25ff74abb2b900dcdfd415ca1f3b4585585ca27
Thanat Mathanukraw <phoon884@gmail.com>
2022-07-25 16:16:15 +0700

    so l

--- test_synthetic.py
+++ test_synthetic.py
@@ -64,7 +64,7 @@ def denoise_syn(data_dir, output_dir):
                 image_raw = Image.open(os.path.join(data_dir, imageName))
                 test = np.random.rand(1980*4,1080*4)
                 print(f"test1:{test.shape}")
-                image_raw = pack_gbrg_raw()#np.array(image_ra
+                image_raw = pack_gbrg_raw(test)#np.array(image_ra
                 print(f"test2:{image_raw.shape}")
                 image_raw = np.random.rand(1920//2, 1080//2, 4).astype(np.float32)
                 print(image_raw.shape)
